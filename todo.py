94d0580d39db459624a69ac43ec24f1e9caadb2f
sam-r7 <uhhh77156@gmail.com>
2025-08-29 11:15:57 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -9,14 +9,20 @@ def add_task(task):
 
 # Step 3: View tasks
 def view_tasks():
-    print(tasks)
+    print("-----")
+    print("To do:")
+    for task in tasks:
+        print(task)
+    print("-----")
+    
 
 # Step 4: Delete a task
-def delete_task(task):
-    tasks.remove(task)
+def delete_task(index):
+    tasks.remove(tasks[index])
 
 # Step 5: Mark task complete
-
+def mark_complete(index):
+    tasks[index] = (tasks[index] + "✅")
 
 # Step 6: Save/load tasks (extra stretch for today)
 
@@ -26,6 +32,7 @@ if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
-   # mark_complete(0)
-   # view_tasks()
+    delete_task(1)
+    mark_complete(0)
+    view_tasks()
    # save_tasks()
